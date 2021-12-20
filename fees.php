77fb1547ab081bed1d9f4dee370ddaa72577b6b2
vilam wu <vilam.wu@virgocx.ca>
2021-12-20 12:34:00 -0500

    fixbug

--- fees.php
+++ fees.php
@@ -624,7 +624,7 @@ get_header();
                             let maximum = entry.maximum;
                             let minimum = entry.minimum;
                             if (typeof maximum !== 'string') {
-                                maximum = maximum === 99999 ? 'Unlimited' : '$' + entry.maximum;
+                                maximum = maximum >= 99999 ? 'Unlimited' : '$' + entry.maximum;
                             }
 
                             if (typeof entry.minimum !== 'string') {
