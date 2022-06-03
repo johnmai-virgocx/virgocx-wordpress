1f5420673a65f5685e03191100e6c306a620a99c
ScottWangVirgoCX <scott.wang@virgocx.ca>
2022-06-03 18:00:24 -0400

    文案修改

--- front-page.php
+++ front-page.php
@@ -598,7 +598,7 @@ if ($totalNumber === 1): ?>
                         <div class="col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center">
                             <img src="<?= get_template_directory_uri() ?>/img/canada2x.png" alt="canada flag" width="20"
                                  height="24"/>
-                            <span class="fint">FINTRAC Registered MSB # M19955733</span>
+                            <span class="fint">FINTRAC Registered MSB # M19955733 Restricted Dealer registered with CSA</span>
                         </div>
                         <!-- <div class="col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center">
                         <span>Assets Protected by <img src="/img/ledger.svg"
