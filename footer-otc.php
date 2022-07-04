09d1ce4c26f2b07e58ef799649cccad67f4a4195
vilam wu <vilam.wu@virgocx.ca>
2022-07-04 16:41:53 -0400

    footer-otc update

--- footer-otc.php
+++ footer-otc.php
@@ -16,20 +16,6 @@
     <div class="container custom-container">
         <div class="footer-tp">
             <div class="row">
-                <!-- <div class="col-lg-2 col-sm-4">
-                    <div class="fp-menu">
-                        <h5 data-translate="footer_col1"></h5>
-                        <?php
-                        wp_nav_menu(
-                            array(
-                                'menu' => 'OTC_footer_col1',
-                                'theme_location' => 'footer-1',
-                                'menu_id' => 'footer-menu-1',
-                            )
-                        );
-                        ?>
-                    </div>
-                </div> -->
                 <div class="col-lg-2 col-sm-4">
                     <div class="fp-menu">
                         <h5 data-translate="OTC_footer_col1"></h5>
@@ -68,9 +54,9 @@
                         <?php
                         wp_nav_menu(
                             array(
-                                'menu' => 'footer-2',
-                                'theme_location' => 'footer-2',
-                                'menu_id' => 'footer-menu-2',
+                                'menu' => 'menu-2',
+                                'theme_location' => 'menu-2',
+                                'menu_id' => 'menu-2',
                             )
                         );
                         ?>
@@ -131,33 +117,6 @@
                                             src="<?= get_template_directory_uri() ?>/img/twitter.png"
                                             alt="images not found"/> <span data-translate="footer_col6_Twitter"></span></a>
                             </li>
-                            <!-- <li>
-                                <a href="https://www.reddit.com/r/VirgoCX/"><img
-                                            src="<?= get_template_directory_uri() ?>/img/Reddit.png"
-                                            alt="images not found"/> <span
-                                            data-translate="footer_col6_Medium"></span></a>
-                            </li>
-                            <li>
-                                <a href="https://www.facebook.com/virgocxofficial"><img
-                                            src="<?= get_template_directory_uri() ?>/img/facebook.png"
-                                            alt="images not found"/> <span data-translate="footer_col6_Facebook"></span></a>
-                            </li>
-                            <li>
-                                <a href="https://www.linkedin.com/company/virgocx/"><img
-                                            src="<?= get_template_directory_uri() ?>/img/linkin.png"
-                                            alt="images not found"/> <span data-translate="footer_col6_LinkedIn"></span></a>
-                            </li>
-                            <li>
-                                <a href="https://www.youtube.com/channel/UCxrH4dmNzxqBnuwD9_DnjTQ"><img
-                                            src="<?= get_template_directory_uri() ?>/img/youtube.png"
-                                            alt="images not found"/> <span data-translate="footer_col6_YouTube"></span></a>
-                            </li>
-                            <li>
-                                <a href="https://www.instagram.com/virgocx_official/"><img
-                                            src="<?= get_template_directory_uri() ?>/img/instagram.png"
-                                            alt="images not found"/> <span
-                                            data-translate="footer_col6_Instagram"></span></a>
-                            </li> -->
                         </ul>
                     </div>
                 </div>
