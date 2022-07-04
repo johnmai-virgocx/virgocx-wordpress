cd5129fd77815eca1173992ec7ad6cf508139440
vilam wu <vilam.wu@virgocx.ca>
2022-07-04 15:44:21 -0400

    otc header & footer update

--- footer-otc.php
+++ footer-otc.php
@@ -16,18 +16,50 @@
     <div class="container custom-container">
         <div class="footer-tp">
             <div class="row">
-                <div class="col-lg-2 col-sm-4">
+                <!-- <div class="col-lg-2 col-sm-4">
                     <div class="fp-menu">
                         <h5 data-translate="footer_col1"></h5>
                         <?php
                         wp_nav_menu(
                             array(
-                                'menu' => 'footer-1',
+                                'menu' => 'OTC_footer_col1',
                                 'theme_location' => 'footer-1',
                                 'menu_id' => 'footer-menu-1',
                             )
                         );
                         ?>
+                    </div>
+                </div> -->
+                <div class="col-lg-2 col-sm-4">
+                    <div class="fp-menu">
+                        <h5 data-translate="OTC_footer_col1"></h5>
+                        <ul>
+                            <li>
+                                <a href="/en-otc" data-translate="OTC_landing_banner2_tag1"></a>
+                            </li>
+                            <li>
+                                <a href="/en-otc-chatbroker" data-translate="OTC_chat_broker_title"></a>
+                            </li>
+                            <li>
+                                <a href="/en-otc-nft-brokerage">NFT</a>
+                            </li>
+                            <li>
+                                <a href="/en-currency-exchange-toronto" data-translate="Foreign Exchange"></a>
+                            </li>
+                            <li>
+                                <a href="https://learn.virgocx.ca/" data-translate="header_Learn"></a>
+                            </li>
+                            <li>
+                                <a href="/en-about-virgocx" data-translate="header_About"></a>
+                            </li>
+                            <li>
+                                <a href="/en-contact" data-translate="Contact"></a>
+                            </li>
+                            <li>
+                                <a href="https://virgocx.zendesk.com/hc/en-us" data-translate="header_Support"></a>
+                            </li>
+                           
+                        </ul>
                     </div>
                 </div>
                 <div class="col-lg-2 col-sm-4">
@@ -46,30 +78,34 @@
                 </div>
                 <div class="col-lg-2 col-sm-4">
                     <div class="fp-menu">
-                        <h5 data-translate="footer_col3"></h5>
-                        <?php
-                        wp_nav_menu(
-                            array(
-                                'menu' => 'footer-3',
-                                'theme_location' => 'footer-3',
-                                'menu_id' => 'footer-menu-3',
-                            )
-                        );
-                        ?>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-sm-4">
-                    <div class="fp-menu">
-                        <h5 data-translate="footer_col4"></h5>
-                        <?php
-                        wp_nav_menu(
-                            array(
-                                'menu' => 'footer-4',
-                                'theme_location' => 'footer-4',
-                                'menu_id' => 'footer-menu-4',
-                            )
-                        );
-                        ?>
+                        <h5 data-translate="OTC_footer_col3"></h5>
+                        <ul>
+                            <li>
+                                <a href="/" data-translate="OTC_home"></a>
+                            </li>
+                            <li>
+                                <a href="/page#/login" data-translate="Log In"></a>
+                            </li>
+                            <li>
+                                <a  href="/page#/register" data-translate="Register"></a>
+                            </li>
+                            <li>
+                                <a href="/tpage/en/bitcoin/price/" data-translate="Prices"></a>
+                            </li>
+                            <li>
+                                <a href="https://status.virgocx.ca/" data-translate="Status"></a>
+                            </li>
+                            <li>
+                                <a href="/en-security" data-translate="footer_col4_Security"></a>
+                            </li>
+                            <li>
+                                <a href="/en-app-download" data-translate="Mobile App"></a>
+                            </li>
+                            <li>
+                                <a href="/en-fees" data-translate="Fees & Funding"></a>
+                            </li>
+                           
+                        </ul>
                     </div>
                 </div>
                 <div class="col-lg-2 col-sm-4">
@@ -129,43 +165,8 @@
         </div>
         <div class="footer-bt">
             <div class="row align-items-center">
-                <div class="col-lg-7 col-md-12">
-                    <div class="fbt-left">
-                        <!-- <ul>
-                            <li class="app-download">
-                                <a class="ios"
-                                   href="https://apps.apple.com/ca/app/virgocx-buy-sell-bitcoin/id1480501048"
-                                   target="_blank">
-                                    <div class="text">
-                                        <span data-translate="footer_App_Apple1"></span>
-                                        <span class="highlight" data-translate="footer_App_Apple2"></span>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="app-download">
-                                <a class="play" href="https://play.google.com/store/apps/details?id=ca.virgocx.exchange"
-                                   target="_blank">
-                                    <div class="text">
-                                        <span data-translate="footer_App_Google1"></span>
-                                        <span class="highlight" data-translate="footer_App_Google2"></span>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="app-download">
-                                <a class="android"
-                                   href="https://virgocx.s3.ca-central-1.amazonaws.com/prod/2021/12/30/861643257275474bbae73bc7a7cabe12.apk"
-                                   target="_blank">
-                                    <div class="text">
-                                        <span data-translate="footer_App_Android1"></span>
-                                        <span class="highlight" data-translate="footer_App_Android2"></span>
-                                    </div>
-                                </a>
-                            </li>
-                        </ul> -->
-                    </div>
-                </div>
                 <div class="col-lg-5 col-md-12">
-                    <div class="newsletter">
+                    <div class="newsletter" id="otc-contact-form">
                         <span data-translate="footer_Stay_in_Touch"></span>
                         <?= do_shortcode('[contact-form-7 id="134" title="Footer newletter otc"]') ?>
                     </div>
@@ -223,10 +224,32 @@
     #otc-body .newsletter .wpcf7-form input {
         color: #B59964;
         font-family: "Matter-Regular";
+        
+    }
+    .email-input{
+        height:55px;
+        font-size:16px !important;
+        line-height:22px !important;
     }
 
     ul li::before {
         content: none;
+    }
+
+    
+    .subcheck-txt{
+	    color:#FFFFFF !important;
+        margin-left:20px;
+        /* white-space:break-word; */
+    }
+    .subCheckBox input[type='checkbox']{
+        width:18px !important;
+        height:18px !important;
+        left:0;
+        /* background-color:#05004d; */
+        padding: 0 !important;
+        border:none !important;
+        border-radius:0 !important;
     }
 
     /*@media screen and (max-width: 576px) {*/
@@ -240,9 +263,6 @@
     .terms {
         height: 70px;
     }
-
-
-
 
     .terms-text {
         position: absolute;
@@ -269,10 +289,44 @@
         left: 0px;
 
     }
+    .subCheckBox{
+        display:flex;
+        flex-direction:row;
+        width:100%;
+        cursor:pointer;
+    }
+    .subCheckBox span{
+        width:18px !important;
+        margin-left:0 !important;
+    }
+    .otc-footer-btn{
+            width: 138px !important;
+            height: 38px !important;
+            border: 2px solid #C7BA9A !important;
+            border-radius: 25px;
+            background-color:#05004D !important;
+            padding:0 !important;
+            vertical-align:middle;
+            /* right:160px !important; */
+            left:0 !important;
+            color:#C7BA9A !important;
+            font-size: 14px;
+            line-height: 17px;
+
+    }
     @media screen and (min-width: 900px) {
         label {
             white-space: nowrap;
         }
+        .footer-bt #otc-contact-form{
+        margin-left:600px !important;
+        margin-top:-150px;  
+        }
+        .subCheckBox{
+            margin-left:-15px;
+        }
+        
+        
     }
 
     @media screen and (max-width: 576px) {
@@ -287,7 +341,11 @@
         #social-links li a{
             text-align: left!important;
         }
-
+        .otc-footer-btn{
+            margin:auto;
+            position: static !important;
+        }
+        
     }
 </style>
 <script>
